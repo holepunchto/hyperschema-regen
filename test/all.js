c2fe4d5ef9f6ab95b864c9914af2fdce9ca2a721
Dominic Cassidy <dominic@cassidy.casa>
2025-11-17 22:06:49 +0200

    simplify and remove async

---
+++ test/all.js
@@ -0,0 +1,29 @@
+const test = require('brittle')
+const { compareSchemas } = require('../')
+
+const schema = require('./fixtures/db-old.json')
+
+test('can compare unchanged schemas', (t) => {
+  compareSchemas(schema, schema)
+  t.pass()
+})
+
+test('fails on changed schemas', (t) => {
+  {
+    const changed = {
+      ...schema,
+      schema: schema.schema.map((s) => ({ ...s, id: s.id + 1 }))
+    }
+
+    t.exception(() => compareSchemas(schema, changed), /Error: IDs do not match: 1 !== 0/)
+  }
+
+  {
+    const changed = {
+      ...schema,
+      schema: schema.schema.map((s) => ({ ...s, namespace: 'fail' }))
+    }
+
+    t.exception(() => compareSchemas(schema, changed), /Error: collection NOT in current schema/)
+  }
+})
